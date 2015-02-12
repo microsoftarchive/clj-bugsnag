59ae02234f4d9227e67524e77eac95ffe84bed50
Torsten Becker <torsten.becker@gmail.com>
2015-02-12 12:22:10 -0500

    Switch URL to https.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-bugsnag "0.1.2"
   :description "Fully fledged Bugsnag client. Supports ex-data and ring middleware."
-  :url "http://github.com/6wunderkinder/clj-bugsnag"
+  :url "https://github.com/6wunderkinder/clj-bugsnag"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.3.0"
