e738ca61c6e0f158353a3c83a24196d76bfa6147
Torsten Becker <torsten.becker@gmail.com>
2015-02-12 10:08:40 -0500

    Add tests.

--- test/clj_bugsnag/core_test.clj
+++ test/clj_bugsnag/core_test.clj
@@ -1,4 +1,13 @@
 (ns clj-bugsnag.core-test
   (:require [midje.sweet :refer :all]
-            [clj-bugsnag.core :refer :all]))
+            [clj-bugsnag.core :as core]))
 
+(fact "includes ExceptionInfo's ex-data"
+  (-> (core/post-data (ex-info "BOOM" {:wat "?!"}) {})
+    :events first (get-in [:metaData "ex–data" ":wat"]))
+  => "?!")
+
+(fact "converts metadata values to strings"
+  (-> (core/post-data (ex-info "BOOM" {}) {:meta {:reason println}})
+    :events first (get-in [:metaData ":reason"]))
+  => (has-prefix "clojure.core$println@"))
