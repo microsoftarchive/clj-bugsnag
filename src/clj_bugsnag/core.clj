b7ba36815ddd9219360215dec569fc4e8695b97d
Torsten Becker <torsten.becker@gmail.com>
2015-02-13 22:10:02 -0500

    Bump version to 0.2.1.

--- src/clj_bugsnag/core.clj
+++ src/clj_bugsnag/core.clj
@@ -53,7 +53,7 @@
                     {})]
     {:apiKey (:api-key data (env :bugsnag-key))
      :notifier {:name "clj-bugsnag"
-                :version "0.2.0"
+                :version "0.2.1"
                 :url "https://github.com/6wunderkinder/clj-bugsnag"}
      :events [{:payloadVersion "2"
                :exceptions [{:errorClass class-name
