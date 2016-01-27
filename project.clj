98146a042ee92a397fd88a8d1aa620529a5710a7
Andrei Ursan <hello@andreiursan.com>
2016-01-27 20:58:56 +0100

    updated the lib v0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-bugsnag "0.2.3"
+(defproject clj-bugsnag "0.2.4"
   :description "Fully fledged Bugsnag client. Supports ex-data and ring middleware."
   :url "https://github.com/wunderlist/clj-bugsnag"
   :license {:name "Eclipse Public License"
@@ -6,10 +6,11 @@
   :min-lein-version "2.3.0"
   :dependencies [
     [clj-stacktrace "0.2.7"]
-    [clj-http "0.9.2"]
+    [clj-http "1.1.2"]
     [environ "0.2.1"]
     [org.clojure/data.json "0.2.4"]
     [org.clojure/clojure "1.6.0"]]
+
   :aliases {
     "test" ["midje"]}
   :profiles {
